b0be3fd7ff70a774f2c7c95183ed5eea2025a6cd
Extreme-S <1716224950@qq.com>
2021-11-18 11:38:37 +0800

    定义自定义异常

---
+++ dcloud-common/src/main/java/org/example/exception/BizException.java
@@ -0,0 +1,28 @@
+package org.example.exception;
+
+import lombok.Data;
+import org.example.enums.BizCodeEnum;
+
+
+@Data
+public class BizException extends RuntimeException {
+
+    private int code;
+
+    private String msg;
+
+    public BizException(Integer code, String message) {
+        super(message);
+        this.code = code;
+        this.msg = message;
+    }
+
+
+    public BizException(BizCodeEnum bizCodeEnum) {
+        super(bizCodeEnum.getMessage());
+        this.code = bizCodeEnum.getCode();
+        this.msg = bizCodeEnum.getMessage();
+    }
+
+
+}
---
+++ dcloud-common/src/main/java/org/example/exception/CustomExceptionHandler.java
@@ -0,0 +1,29 @@
+package org.example.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.util.JsonData;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+//@RestControllerAdvice
+@Slf4j
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public JsonData handler(Exception e) {
+
+        if (e instanceof BizException) {
+            BizException bizException = (BizException) e;
+            log.error("[业务异常]{}", e);
+            return JsonData.buildCodeAndMsg(bizException.getCode(), bizException.getMsg());
+        } else {
+            log.error("[系统异常]{}", e);
+            return JsonData.buildError("系统异常");
+        }
+
+    }
+
+}
