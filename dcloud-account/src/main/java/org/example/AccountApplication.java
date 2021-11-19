6d370841ede71b856c882994d6271e2c10f916ab
Extreme-S <1716224950@qq.com>
2021-11-19 09:28:11 +0800

    服务注册发现

---
+++ dcloud-account/src/main/java/org/example/AccountApplication.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@MapperScan("org.example.mapper")
+@EnableTransactionManagement
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AccountApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class, args);
+    }
+
+}
