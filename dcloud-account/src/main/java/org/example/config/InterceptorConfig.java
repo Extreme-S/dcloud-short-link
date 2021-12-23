e15ffe872369ecf0727fa5756a0083e999d80a71
Extreme-S <1716224950@qq.com>
2021-12-23 17:08:40 +0800

    interceptor拦截器配置

---
+++ dcloud-account/src/main/java/org/example/config/InterceptorConfig.java
@@ -0,0 +1,24 @@
+package org.example.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Slf4j
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(new LoginInterceptor())
+            //添加拦截的路径
+            .addPathPatterns("/api/account/*/**", "/api/traffic/*/**")
+
+            //排除不拦截
+            .excludePathPatterns("/api/account/*/register", "/api/account/*/upload", "/api/account/*/login",
+                "/api/notify/v1/captcha", "/api/notify/*/send_code");
+    }
+}
---
+++ dcloud-common/src/main/java/org/example/interceptor/LoginInterceptor.java
@@ -0,0 +1,79 @@
+package org.example.interceptor;
+
+import io.jsonwebtoken.Claims;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.example.enums.BizCodeEnum;
+import org.example.model.LoginUser;
+import org.example.util.CommonUtil;
+import org.example.util.JWTUtil;
+import org.example.util.JsonData;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Slf4j
+public class LoginInterceptor implements HandlerInterceptor {
+
+    public static ThreadLocal<LoginUser> threadLocal = new ThreadLocal<>();
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+        throws Exception {
+        if (HttpMethod.OPTIONS.toString().equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpStatus.NO_CONTENT.value());
+            return true;
+        }
+        //获取accessToken
+        String accessToken = request.getHeader("token");
+        if (StringUtils.isBlank(accessToken)) {
+            accessToken = request.getParameter("token");
+        }
+
+        if (StringUtils.isNotBlank(accessToken)) {
+            Claims claims = JWTUtil.checkJWT(accessToken);
+            if (claims == null) {//未登录
+                CommonUtil.sendJsonMessage(response, JsonData.buildResult(BizCodeEnum.ACCOUNT_UNLOGIN));
+                return false;
+            }
+            long accountNo = Long.parseLong(claims.get("account_no").toString());
+            String headImg = (String) claims.get("head_img");
+            String username = (String) claims.get("username");
+            String mail = (String) claims.get("mail");
+            String phone = (String) claims.get("phone");
+            String auth = (String) claims.get("auth");
+
+            LoginUser loginUser = LoginUser.builder()
+                .accountNo(accountNo)
+                .auth(auth)
+                .phone(phone)
+                .headImg(headImg)
+                .mail(mail)
+                .username(username)
+                .build();
+
+            //通过Attribute
+            //request.setAttribute("loginUser",loginUser);
+
+            //通过threadlocal
+            threadLocal.set(loginUser);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+        ModelAndView modelAndView) throws Exception {
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+        throws Exception {
+        threadLocal.remove();
+    }
+}
