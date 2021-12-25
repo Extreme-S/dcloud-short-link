7376762d6548a9ac8d8dc154fdd9e30c95b8ec55
Extreme-S <1716224950@qq.com>
2021-12-25 14:48:00 +0800

    sharding分表开发配置完成，测试成功

---
+++ dcloud-account/src/test/java/org/example/biz/TrafficTest.java
@@ -0,0 +1,36 @@
+package org.example.biz;
+
+import java.util.Random;
+import lombok.extern.slf4j.Slf4j;
+import org.example.AccountApplication;
+import org.example.mapper.TrafficMapper;
+import org.example.model.TrafficDO;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = AccountApplication.class)
+@Slf4j
+public class TrafficTest {
+
+
+    @Autowired
+    private TrafficMapper trafficMapper;
+
+
+    @Test
+    public void testSaveTraffic() {
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            TrafficDO trafficDO = new TrafficDO();
+            trafficDO.setAccountNo((long) random.nextInt(100));
+            trafficMapper.insert(trafficDO);
+        }
+    }
+
+
+}
