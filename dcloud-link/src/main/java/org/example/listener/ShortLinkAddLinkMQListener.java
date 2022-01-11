3b1e3ec38f5c6a590b032abe8c15e1ae2eb98bb7
Extreme-S <1716224950@qq.com>
2022-01-11 21:06:03 +0800

    MQ消费者异常重试处理方案测试

--- dcloud-link/src/main/java/org/example/listener/ShortLinkAddLinkMQListener.java
+++ dcloud-link/src/main/java/org/example/listener/ShortLinkAddLinkMQListener.java
@@ -7,19 +7,19 @@ import org.example.enums.BizCodeEnum;
 import org.example.exception.BizException;
 import org.example.model.EventMessage;
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
-@RabbitListener(queues = "short_link.add.link.queue")
+@RabbitListener(queuesToDeclare = {@Queue("short_link.add.link.queue")})
 public class ShortLinkAddLinkMQListener {
 
     @RabbitHandler
     public void shortLinkHandler(EventMessage eventMessage, Message message, Channel channel) throws IOException {
         log.info("监听到消息ShortLinkAddLinkMQListener message消息内容:{}", message);
-        long tag = message.getMessageProperties().getDeliveryTag();
         try {
             //TODO 处理业务逻辑
         } catch (Exception e) {
--- dcloud-link/src/main/java/org/example/listener/ShortLinkAddMappingMQListener.java
+++ dcloud-link/src/main/java/org/example/listener/ShortLinkAddMappingMQListener.java
@@ -7,22 +7,21 @@ import org.example.enums.BizCodeEnum;
 import org.example.exception.BizException;
 import org.example.model.EventMessage;
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
-@RabbitListener(queues = "short_link.add.mapping.queue")
+@RabbitListener(queuesToDeclare = {@Queue("short_link.add.mapping.queue")})
 public class ShortLinkAddMappingMQListener {
 
     @RabbitHandler
     public void shortLinkHandler(EventMessage eventMessage, Message message, Channel channel) throws IOException {
         log.info("监听到消息ShortLinkAddMappingMQListener message消息内容:{}", message);
-        long tag = message.getMessageProperties().getDeliveryTag();
         try {
             //TODO 处理业务逻辑
-            int i = 1 / 0;
         } catch (Exception e) {
             //处理业务异常，还有进行其他操作，比如记录失败原因
             log.error("消费失败:{}", eventMessage);
